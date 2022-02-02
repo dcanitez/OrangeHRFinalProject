570ed86b51e16cbf95af36beb7cd058d6984a0f9
dcanitez <dilaycanitez@gmail.com>
2022-02-02 02:29:55 +0200

    create permission operations added

--- OrangeHRFinalProject.ViewModels/PermissionViewModels/PermissionCreateVM.cs
+++ OrangeHRFinalProject.ViewModels/PermissionViewModels/PermissionCreateVM.cs
@@ -1,7 +1,13 @@
-﻿namespace OrangeHRFinalProject.ViewModels.PermissionViewModels
+﻿using System;
+
+namespace OrangeHRFinalProject.ViewModels.PermissionViewModels
 {
     public class PermissionCreateVM
     {
-
+        //employee id bilgisini giriş yapan kullanıcıdan almamız lazım.
+        //public int EmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
     }
 }
--- OrangeHRFinalProject/Controllers/PersonelController.cs
+++ OrangeHRFinalProject/Controllers/PersonelController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OrangeHRFinalProject.BLL.ServiceOperations.Interfaces;
+using OrangeHRFinalProject.Entities.Authentication;
+using OrangeHRFinalProject.ViewModels.PermissionViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,37 @@ namespace OrangeHRFinalProject.Controllers
 {
     public class PersonelController: Controller
     {
+        private readonly IPermissionService permissionService;       
+
+        public PersonelController(IPermissionService permissionService)
+        {
+            this.permissionService = permissionService;            
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult CreatePermission()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePermission(int id,PermissionCreateVM model)
+        {            
+            //TODO: ID bir employeeId bilgisini userManager ile yönetmek lazım!
+            if (ModelState.IsValid)
+            {
+                var result = await permissionService.Add(model);
+                if (result is not null)
+                {
+                    //bir mesaj verebiliriz belki* izin oluşturuldu bravo diye
+                    return View();
+                }
+                ModelState.AddModelError(string.Empty, "İzin talebiniz oluşturulamadı");
+            }            
+            return View(model);
         }
     }
 }
